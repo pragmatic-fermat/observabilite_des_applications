fa4a389c183b70e6fa48c2cd86c64e8b2ca39231
Pragmatic Fermat <141491326+pragmatic-fermat@users.noreply.github.com>
2024-12-03 21:37:13 +0100

    fix list

--- tf/main.tf
+++ tf/main.tf
@@ -80,5 +80,5 @@ resource "digitalocean_project" "training" {
   description = "Projet pour la formation"
   purpose     = "Web Application"
   environment = "Development"
-  resources   = digitalocean_droplet.clt[*].urn , digitalocean_droplet.srv[*].urn
+  resources   = [ digitalocean_droplet.clt[*].urn , digitalocean_droplet.srv[*].urn ]
 }
