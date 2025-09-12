86656f4a9dcf415836b2f685929e8fdaf1f202b9
Pragmatic Fermat <141491326+pragmatic-fermat@users.noreply.github.com>
2025-09-12 17:21:47 +0200

    Update new-messenger-tracing.mjs

--- data/new-messenger-tracing.mjs
+++ data/new-messenger-tracing.mjs
@@ -1,12 +1,22 @@
 // tracing.mjs
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base";
+import { Resource } from "@opentelemetry/resources";import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+
+const resource = new Resource({  [SemanticResourceAttributes.SERVICE_NAME]: "messenger",});
 
 const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
+	resource,
+  traceExporter: new OTLPTraceExporter({ headers: {} }),
   instrumentations: [getNodeAutoInstrumentations()],
-  spanLimits: { attributeValueLengthLimit: 256, attributeCountLimit: 64 },
+  spanLimits: {
+    attributeCountLimit: 64,
+    attributeValueLengthLimit: 512,
+    eventCountLimit: 128,
+    eventAttributeCountLimit: 32,
+  },
 });
 
 await sdk.start();
